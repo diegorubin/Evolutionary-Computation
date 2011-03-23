a5700f3fc73ca77071eb182eda4df58d4621f6e8
Diego Rubin <drubin@ciandt.com>
2011-03-23 11:51:54 -0300

    Gerando populacao corretamente.

--- problem1/ruby/lib/population.rb
+++ problem1/ruby/lib/population.rb
@@ -1,25 +1,30 @@
 #require File.join(File.dirname(__FILE__),'individual')
 
 class Population
+  attr_accessor :generation, :individuals
   def self.generate
-    @generation = 1
-    @individuals = []
+    population = Population.new
+    population.generation = 1
+    individuals = []
     begin
       individual = Individual.new(:x => rand(19),
                                   :y => rand(9),
                                   :z => rand(4))
-      @individuals << individual if individual.is_possible?
-    end until(@individuals.size == 100)
-    self
+      individuals << individual if individual.is_possible?
+    end until(individuals.size == 100)
+    population.individuals = individuals
+    population
   end
 
-  def show
+  def show(stdout = false)
     result = File.open("generation#{@generation}.dat",'w')
     
     @individuals.each_with_index do |individual,i|
-      values = []
-      individual.v.each{|k,v| values << "#{k} -> #{v}"}
-      puts "#{i+1}: #{values.join(', ')} = #{individual.f}"
+      if stdout
+        values = []
+        individual.v.each{|k,v| values << "#{k} -> #{v}"}
+        puts "#{i+1}: #{values.join(', ')} = #{individual.f}"
+      end
       result.puts "#{i+1} #{individual.f}"
     end
     
